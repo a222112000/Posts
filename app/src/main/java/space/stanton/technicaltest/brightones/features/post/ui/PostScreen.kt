d25246c1950b86becab7456f7e64d896fa40143d
Alselwi <a222112000@yahoo.com>
2023-10-06 10:33:10 +0100

    added loading and error features in the postscreen

--- app/src/main/java/space/stanton/technicaltest/brightones/features/post/ui/PostScreen.kt
+++ app/src/main/java/space/stanton/technicaltest/brightones/features/post/ui/PostScreen.kt
@@ -2,12 +2,14 @@ package space.stanton.technicaltest.brightones.features.post.ui
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -17,8 +19,10 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -63,6 +67,26 @@ fun PostView(
                         PostView(post = it, postViewModel = postViewModel)
                         Divider(modifier = Modifier.padding(top = 8.dp, bottom = 16.dp))
                     }
+                }
+            }
+            if (postViewModel.posts.value.error.isNotBlank()) {
+                Text(
+                    text = postViewModel.posts.value.error,
+                    color = MaterialTheme.colorScheme.error,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp)
+
+                )
+            }
+
+            if (postViewModel.posts.value.isLoading) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
                 }
             }
         }
